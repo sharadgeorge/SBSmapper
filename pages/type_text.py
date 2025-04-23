e8d24bd09ae2276e72c6c2d0d61d4de74f795b64
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-23 20:02:50 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -31,7 +31,10 @@ numMAPPINGS_input = 5
 #st.button("Clear text", on_click=on_click)
 
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+#model = SentenceTransformer('all-MiniLM-L6-v2') # fastest
+model = SentenceTransformer("all-mpnet-base-v2") # best performance
+#model = SentenceTransformers("all-distilroberta-v1")
+
 INTdesc_embedding = model.encode(INTdesc_input)
 
 # Compute cosine similarity between all pairs of SBS descriptions
