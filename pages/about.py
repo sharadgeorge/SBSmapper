f777165d1a435f8195ba9a4246ed443b7248b33a
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-23 16:55:07 +0300

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -6,7 +6,7 @@ with st.expander("ℹ️ - About this app", expanded=True):
 
     st.write(
         """     
--   This app maps internal codes to the corresponding SBS codes. 
+-   This app maps internal descriptions to the corresponding SBS codes. 
 -   myDemo model was developed from xxxxxxxx in HuggingFace, and fine-tuned on xxxxx (xxxx). 
 -   The model uses the default pretrained tokenizer.
        """
