fcd4acb2ca796b0966b03749287fed1e34a6f3d8
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-23 21:05:01 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -75,7 +75,8 @@ dictA = {"word": [], "entity": []}
 dictB = {"word": [], "entity": []}
 
 
-df_SBS = pd.read_csv("SBS_V2_Code_list.xlsx", index_col=1, na_values=['NA'], usecols=[3])
+#df_SBS = pd.read_csv("SBS_V2_Code_list.xlsx", index_col=1, na_values=['NA'], usecols=[3])
+df_SBS = pd.read_csv("SBS_V2_Code_list.xlsx")
 print(df_SBS.header)
 
 if INTdesc_input is not None and createSBScodes_button == True: 
