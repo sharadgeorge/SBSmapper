fabf43d0cffe4e8df8bbedbdd2334eaae07ffd7c
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-23 17:11:26 +0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -4,28 +4,29 @@ st.title("📘SBS mapper")
 
 st.header("Internal descriptions can be mapped to SBS codes in the below chapters (work in progress)")
 
-Procedures on nervous system
-Procedures on endocrine system
-Procedures on eye and adnexa
-Procedures on ear and mastoid process
-Procedures on nose, mouth and pharynx
-Dental services
-Procedures on respiratory system
-Procedures on cardiovascular system
-Procedures on blood and blood-forming organs
-Procedures on digestive system
-Procedures on urinary system
-Procedures on male genital organs
-Gynaecological procedures
-Obstetric procedures
-Procedures on musculoskeletal system
-Dermatological and plastic procedures
-Procedures on breast
-Radiation oncology procedures
-Non-invasive, cognitive and other interventions, not elsewhere classified   
-Imaging services
-Laboratory and Pathology Services
-Ambulance and Transport Services
-KSA Service Codes
-Emergency Medical Services (EMS)
-Mortuary Services
+'Procedures on nervous system'
+'Procedures on endocrine system'
+'Procedures on eye and adnexa'
+'Procedures on ear and mastoid process'
+'Procedures on nose, mouth and pharynx'
+'Dental services'
+'Procedures on respiratory system'
+'Procedures on cardiovascular system'
+'Procedures on blood and blood-forming organs'
+'Procedures on digestive system'
+'Procedures on urinary system'
+'Procedures on male genital organs'
+'Gynaecological procedures'
+'Obstetric procedures'
+'Procedures on musculoskeletal system'
+'Dermatological and plastic procedures'
+'Procedures on breast'
+'Radiation oncology procedures'
+'Non-invasive, cognitive and other interventions, not elsewhere classified'
+'Imaging services'
+'Laboratory and Pathology Services'
+'Ambulance and Transport Services'
+'KSA Service Codes'
+'Emergency Medical Services (EMS)'
+'Mortuary Services'
+
