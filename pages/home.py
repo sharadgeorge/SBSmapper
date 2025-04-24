b13b83aa5891f42e3bc61c85fa71d26fa2d2d06c
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-24 05:43:39 +0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -4,8 +4,8 @@ st.title("📘SBS mapper")
 
 st.header("Internal descriptions can be mapped to SBS codes in the below chapters (work in progress)")
 
-#from IPython.display import Image
-#Image("SBSmapper/images/SBS_Chapters.png")
+from IPython.display import Image
+Image("images/SBS_Chapters.png")
 
 'Procedures on nervous system'
 'Procedures on endocrine system'
