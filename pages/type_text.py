f6264ad6fc78f604250d817b4cc26f904212a6a4
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-25 22:38:05 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -105,12 +105,12 @@ if INTdesc_input is not None and createSBScodes_clicked == True:
                         
             dfA = pd.DataFrame.from_dict(dictA) 
 
-        question = "Which, if any, of the above SBS descriptions corresponds best to " + INTdesc_input +"?" 
-        shortlist = [SBScorpus[result[0]["corpus_id"]], SBScorpus[result[1]["corpus_id"]], SBScorpus[result[2]["corpus_id"]], SBScorpus[result[3]["corpus_id"]], SBScorpus[result[4]["corpus_id"]]] 
-        prompt = [question + str(item) for item in shortlist]
-        st.write(prompt)
-        #st.write(qa_model(question = question, context = shortlist[0]))
-        #st.write(reasoning_model(question + shortlist)
+    question = "Which, if any, of the above SBS descriptions corresponds best to " + INTdesc_input +"?" 
+    shortlist = [SBScorpus[result[0]["corpus_id"]], SBScorpus[result[1]["corpus_id"]], SBScorpus[result[2]["corpus_id"]], SBScorpus[result[3]["corpus_id"]], SBScorpus[result[4]["corpus_id"]]] 
+    prompt = question + [str(item) for item in shortlist]
+    st.write(prompt)
+    #st.write(qa_model(question = question, context = shortlist[0]))
+    #st.write(reasoning_model(question + shortlist)
      
     bs, b1, b2, b3, bLast = st.columns([0.75, 1.5, 1.5, 1.5, 0.75])
     with b1:
