8542821ed39a8e19dc91b0caf8dc500bf7be4fc8
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-25 21:35:18 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -106,7 +106,7 @@ if INTdesc_input is not None and createSBScodes_clicked == True:
             question = "Which, if any, of the above SBS descriptions corresponds best to " + INTdesc_input +"?" 
             st.write(question)    
             shortlist = [SBScorpus[result[0]["corpus_id"]], SBScorpus[result[1]["corpus_id"]], SBScorpus[result[2]["corpus_id"]], SBScorpus[result[3]["corpus_id"]], SBScorpus[result[4]["corpus_id"]]] 
-            st.write(qa_model(question = question, context = [item for item in shortlist]))
+            st.write(qa_model(question = question, context = shortlist[0]))
             #st.write(best_mapping)
      
     bs, b1, b2, b3, bLast = st.columns([0.75, 1.5, 1.5, 1.5, 0.75])
