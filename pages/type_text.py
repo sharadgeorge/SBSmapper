725685965a4b70703ed793f199c3dd97ae6f6a64
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-04-30 19:04:04 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -3,17 +3,19 @@ import pandas as pd
 from io import StringIO
 import json
 import torch
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM #AutoModelForTokenClassification
+from transformers import pipeline # AutoTokenizer, AutoModelForCausalLM, AutoModelForTokenClassification
 from sentence_transformers import SentenceTransformer, util 
-#import lmdeploy
-#import turbomind as tm 
 
-from huggingface_hub import login
-login(token = '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')
+import os
+os.getenv("HF_TOKEN")
+
+def get_device_map() -> str:
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
+device = get_device_map()  # 'cpu'
 
 def on_click():
     st.session_state.user_input = ""
-
+    
 #@st.cache
 def convert_df(df:pd.DataFrame):
      return df.to_csv(index=False).encode('utf-8')
@@ -26,8 +28,6 @@ def convert_json(df:pd.DataFrame):
     #st.json(json_string, expanded=True)
     return json_string
 
-#st.title("📘SBS mapper")
-
 INTdesc_input = st.text_input("Type internal description and hit Enter", key="user_input") 
 
 createSBScodes, right_column = st.columns(2)
@@ -38,6 +38,11 @@ numMAPPINGS_input = 5
 #numMAPPINGS_input = st.text_input("Type number of mappings and hit Enter", key="user_input_numMAPPINGS")
 #st.button("Clear text", on_click=on_click)
 
+@st.cache_resource
+def load_model():
+    model = SentenceTransformer('all-MiniLM-L6-v2') # fastest
+    return model
+#load_model() 
 
 model = SentenceTransformer('all-MiniLM-L6-v2') # fastest
 #model = SentenceTransformer('all-mpnet-base-v2') # best performance
@@ -66,73 +71,57 @@ HF_model_results = util.semantic_search(INTdesc_embedding, SBScorpus_embeddings)
 HF_model_results_sorted = sorted(HF_model_results, key=lambda x: x[1], reverse=True)
 HF_model_results_displayed = HF_model_results_sorted[0:numMAPPINGS_input]
 
-#model_id = "meta-llama/Llama-3.2-1B-Instruct"
-#pipe = pipeline("text-generation", model=model_id, torch_dtype=torch.bfloat16, device_map="auto",)
+#@st.cache_resource
+#def load_model_pipe():
+#    pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", device_map=device,) # device_map="auto", torch_dtype=torch.bfloat16 
+#    return pipe 
+#load_model_pipe()
 
-col1, col2, col3 = st.columns([1,1,2.5])
-col1.subheader("Score")
-col2.subheader("SBS code")
-col3.subheader("SBS description V2.0")
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", device_map=device,) # device_map="auto", torch_dtype=torch.bfloat16 
 
 dictA = {"Score": [], "SBS Code": [], "SBS Description V2.0": []}
+dfALL = pd.DataFrame.from_dict(dictA)
 
 if INTdesc_input is not None and createSBScodes_clicked == True: 
     #for i, result in enumerate(HF_model_results_displayed):
     for result in HF_model_results_displayed:
-        with st.container():
-            col1.write("%.4f" % result[0]["score"])
-            col2.write(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[0]["corpus_id"]],"SBS_Code_Hyphenated"].values[0])
-            col3.write(SBScorpus[result[0]["corpus_id"]])
-            dictA["Score"].append("%.4f" % result[0]["score"]), dictA["SBS Code"].append(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[0]["corpus_id"]],"SBS_Code_Hyphenated"].values[0]), dictA["SBS Description V2.0"].append(SBScorpus[result[0]["corpus_id"]])
-            
-            col1.write("%.4f" % result[1]["score"])
-            col2.write(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[1]["corpus_id"]],"SBS_Code_Hyphenated"].values[0])
-            col3.write(SBScorpus[result[1]["corpus_id"]])
-            dictA["Score"].append("%.4f" % result[1]["score"]), dictA["SBS Code"].append(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[1]["corpus_id"]],"SBS_Code_Hyphenated"].values[0]), dictA["SBS Description V2.0"].append(SBScorpus[result[1]["corpus_id"]])
-            
-            col1.write("%.4f" % result[2]["score"])
-            col2.write(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[2]["corpus_id"]],"SBS_Code_Hyphenated"].values[0])
-            col3.write(SBScorpus[result[2]["corpus_id"]])
-            dictA["Score"].append("%.4f" % result[2]["score"]), dictA["SBS Code"].append(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[2]["corpus_id"]],"SBS_Code_Hyphenated"].values[0]), dictA["SBS Description V2.0"].append(SBScorpus[result[2]["corpus_id"]])
-            
-            col1.write("%.4f" % result[3]["score"])
-            col2.write(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[3]["corpus_id"]],"SBS_Code_Hyphenated"].values[0])
-            col3.write(SBScorpus[result[3]["corpus_id"]])
-            dictA["Score"].append("%.4f" % result[3]["score"]), dictA["SBS Code"].append(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[3]["corpus_id"]],"SBS_Code_Hyphenated"].values[0]), dictA["SBS Description V2.0"].append(SBScorpus[result[3]["corpus_id"]])
-            
-            col1.write("%.4f" % result[4]["score"])
-            col2.write(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[4]["corpus_id"]],"SBS_Code_Hyphenated"].values[0])
-            col3.write(SBScorpus[result[4]["corpus_id"]])
-            dictA["Score"].append("%.4f" % result[4]["score"]), dictA["SBS Code"].append(df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[4]["corpus_id"]],"SBS_Code_Hyphenated"].values[0]), dictA["SBS Description V2.0"].append(SBScorpus[result[4]["corpus_id"]])
-                        
-            dfA = pd.DataFrame.from_dict(dictA) 
+        dictA.update({"Score": "%.4f" % result[i]["score"], "SBS Code": df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[i]["corpus_id"]],"SBS_Code_Hyphenated"].values[0], "SBS Description V2.0": SBScorpus[result[i]["corpus_id"]]})
+        dfALL = pd.concat([dfALL, pd.DataFrame([dictA])], ignore_index=True)
+        #dictA.update({"Score": "%.4f" % result[1]["score"], "SBS Code": df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[1]["corpus_id"]],"SBS_Code_Hyphenated"].values[0], "SBS Description V2.0": SBScorpus[result[1]["corpus_id"]]})
+        #dfALL = pd.concat([dfALL, pd.DataFrame([dictA])], ignore_index=True)
+        #dictA.update({"Score": "%.4f" % result[2]["score"], "SBS Code": df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[2]["corpus_id"]],"SBS_Code_Hyphenated"].values[0], "SBS Description V2.0": SBScorpus[result[2]["corpus_id"]]})
+        #dfALL = pd.concat([dfALL, pd.DataFrame([dictA])], ignore_index=True)
+        #dictA.update({"Score": "%.4f" % result[3]["score"], "SBS Code": df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[3]["corpus_id"]],"SBS_Code_Hyphenated"].values[0], "SBS Description V2.0": SBScorpus[result[3]["corpus_id"]]})
+        #dfALL = pd.concat([dfALL, pd.DataFrame([dictA])], ignore_index=True)
+        #dictA.update({"Score": "%.4f" % result[4]["score"], "SBS Code": df_SBS.loc[df_SBS["Long_Description"] == SBScorpus[result[4]["corpus_id"]],"SBS_Code_Hyphenated"].values[0], "SBS Description V2.0": SBScorpus[result[4]["corpus_id"]]})
+        #dfALL = pd.concat([dfALL, pd.DataFrame([dictA])], ignore_index=True)
+        
+        st.dataframe(data=dfALL, hide_index=True)
 
-    display_format = "ask REASONING MODEL: Which, if any, of the above Saudi Billing System descriptions corresponds best to " + INTdesc_input +"? " 
+    display_format = "ask REASONING MODEL: Which, if any, of the above SBS descriptions corresponds best to " + INTdesc_input +"? " 
     st.write(display_format)
-    st.markdown("Model answer needs to be integrated here (Work in progress)")
     question = "Which, if any, of the below Saudi Billing System descriptions corresponds best to " + INTdesc_input +"? " 
     shortlist = [SBScorpus[result[0]["corpus_id"]], SBScorpus[result[1]["corpus_id"]], SBScorpus[result[2]["corpus_id"]], SBScorpus[result[3]["corpus_id"]], SBScorpus[result[4]["corpus_id"]]] 
     prompt = [question + " " + shortlist[0] + " " + shortlist[1] + " " + shortlist[2] + " " + shortlist[3] + " " + shortlist[4]]
     #st.write(prompt)
     
-    #messages = [
-    #{"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    #{"role": "user", "content": "Who are you?"},
-    #]
-    #outputs = pipe(
-    #    messages,
-    #    max_new_tokens=256,
-    #)
-    #st.write(outputs[0]["generated_text"][-1])
+    messages = [
+    {"role": "system", "content": "You are a knowledgable AI assistant who always answers truthfully and precisely!"},
+    {"role": "user", "content": prompt},
+        ]
+    outputs = pipe(
+        messages,
+        max_new_tokens=256,
+    )
+    st.write(outputs[0]["generated_text"][-1]["content"])
     
     bs, b1, b2, b3, bLast = st.columns([0.75, 1.5, 1.5, 1.5, 0.75])
     with b1:
         #csvbutton = download_button(results, "results.csv", "📥 Download .csv")
-        csvbutton = st.download_button(label="📥 Download .csv", data=convert_df(dfA), file_name= "results.csv", mime='text/csv', key='csv_b')
+        csvbutton = st.download_button(label="📥 Download .csv", data=convert_df(dfALL), file_name= "results.csv", mime='text/csv', key='csv_b')
     with b2:
         #textbutton = download_button(results, "results.txt", "📥 Download .txt")
-        textbutton = st.download_button(label="📥 Download .txt", data=convert_df(dfA), file_name= "results.text", mime='text/plain',  key='text_b')
+        textbutton = st.download_button(label="📥 Download .txt", data=convert_df(dfALL), file_name= "results.text", mime='text/plain',  key='text_b')
     with b3:
         #jsonbutton = download_button(results, "results.json", "📥 Download .json")
-        jsonbutton = st.download_button(label="📥 Download .json", data=convert_json(dfA), file_name= "results.json", mime='application/json',  key='json_b')
-
+        jsonbutton = st.download_button(label="📥 Download .json", data=convert_json(dfALL), file_name= "results.json", mime='application/json',  key='json_b') 
