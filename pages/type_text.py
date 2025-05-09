08cb1c95ff594c0c66da9442b5cfad41a7287e09
sharadgeorge <68074475+sharadgeorge@users.noreply.github.com>
2025-05-09 09:43:52 +0300

    Update type_text.py

--- pages/type_text.py
+++ pages/type_text.py
@@ -7,46 +7,18 @@ from transformers import pipeline # AutoTokenizer, AutoModelForCausalLM, AutoMod
 from sentence_transformers import SentenceTransformer, util 
 import time
 import os
-#os.getenv("HF_TOKEN") 
+os.getenv("HF_TOKEN") 
 
-from streamlit_scroll_navigation import scroll_navbar
-
-# Anchor IDs and icons
-anchor_ids = ["About", "Features", "Settings", "Pricing", "Contact"]
-anchor_icons = ["info-circle", "lightbulb", "gear", "tag", "envelope"]
-
-# 1. as sidebar menu
-#with st.sidebar:
-#    st.subheader("Example 1")
-#    scroll_navbar(
-#        anchor_ids,
-#        anchor_labels=None, # Use anchor_ids as labels
-#        anchor_icons=anchor_icons)
-
-# 2. horizontal menu
-st.subheader("Example 2")
-scroll_navbar(
-        anchor_ids,
-        key = "navbar2",
-        anchor_icons=anchor_icons,
-        orientation="horizontal",
-        disable_scroll=True)
-
-# Dummy page setup
-for anchor_id in anchor_ids:
-    st.subheader(anchor_id,anchor=anchor_id)
-    st.write("content " * 100)
-
-#hide_streamlit_style = """
-#        <style>
-#        div[data-testid="stHeader"] {
-#        visibility: visible;
-#        position: sticky;
-#        top: 0
-#        }
-#        </style>
-#        """
-#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+        <style>
+        div[data-testid="stHeader"] {
+        visibility: visible;
+        position: sticky;
+        top: 0
+        }
+        </style>
+        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.header("Map internal descriptions to SBS codes with        Sentence Transformer + Reasoning Models")
 st.subheader("Select specific Chapter for quicker results")
